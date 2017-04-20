0171b0a5b474e2ef2c640e22aacf0de883bbe521
kohlbrr <kohlbrr@gmail.com>
2017-04-20 16:27:57 -0400

    Merge sort added

--- sorting.js
+++ sorting.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function bubbleSort(arr) {
   // If arr i+1 > arr i
   //  swap
@@ -20,4 +22,39 @@ function swap(arr, a, b) {
   arr[a] = arr[b];
   arr[b] = c;
   return arr;
+};
+
+function mergeSort(arr) {
+  if(arr.length < 2) return arr;
+  var splits = split(arr),
+      left = splits[0],
+      right = splits[1];
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+function split(arr) {
+  var center = arr.length / 2;
+  var left = arr.slice(0, center);
+  var right = arr.slice(center);
+  return [left, right];
+};
+
+function merge(left, right) {
+  var merged = [],
+      li = 0,
+      ri = 0;
+  while(li < left.length && ri < right.length) {
+    if(left[li] < right[ri]) {
+      merged.push(left[li]);
+      li++;
+    } else {
+      merged.push(right[ri]);
+      ri++;
+    };
+  };
+  //for(; li < left.length; li++) merged.push(left[li]);
+  //for(; ri < right.length; ri++) merged.push(right[ri]);
+  while(li < left.length) merged.push(left[li++]);
+  while(ri < right.length) merged.push(right[ri++]);
+  return merged;
 };
